5e1e8babc7d5627ffd566054ee24c05e9113ea12
Pankaj Kumar Dadhich <dadhichp@hcl.com>
2017-11-29 15:27:37 +0530

    initial application

--- app/src/main/java/com/example/dadhichp/pankajapplication/MainActivity.java
+++ app/src/main/java/com/example/dadhichp/pankajapplication/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.dadhichp.pankajapplication;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        Dialog dlg = new Dialog(view.getContext());
+
+        dlg.show();
+
+
     }
 }
